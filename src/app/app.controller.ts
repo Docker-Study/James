d9ba242c64f7333e3ec11708006c364c8ff7fa50
jinseo-James-Cha <jinseo_cha@hotmail.com>
2021-11-08 23:47:08 +0900

    2021-11-08 add redis simple example

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,12 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { CacheInterceptor, CACHE_MANAGER, Controller, Get, Inject, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Cache } from 'cache-manager';
 
+@UseInterceptors(CacheInterceptor)
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService,
+     @Inject(CACHE_MANAGER) private cacheManager: Cache) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get("/cache")
+  async getCache(): Promise<string> {
+    const savedTime = await this.cacheManager.get<number>('time')
+    if( savedTime ){
+      return "saved time : " + savedTime
+    }
+    const now = new Date().getTime()
+    await this.cacheManager.set<number>('time',now);
+    return "save new time : " + now
   }
 }
