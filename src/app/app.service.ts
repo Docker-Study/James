abe26e944057ba23eec4b8cded41a1443b71ed5e
jinseo-James-Cha <jinseo_cha@hotmail.com>
2021-11-16 00:36:47 +0900

    2021-11-16 test travis ci set up

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello world, James!';
+    return 'Hello world, James!!!';
   }
 }
